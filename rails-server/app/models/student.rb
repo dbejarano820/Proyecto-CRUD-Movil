d39d6acbfdfa5c259a8d106e43b07ebf898b6079
Daniel Bejarano <dbejarano820@gmail.com>
2023-04-01 15:23:15 -0600

    students controller and model

--- rails-server/app/models/student.rb
+++ rails-server/app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
-end
+    has_many :student_courses
+    has_many :courses, through: :student_courses
+  
+    validates :name, presence: true
+    validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  end
+  
