afe6d3e29ff9b314793b321826898b123f8755a6
Daniel Bejarano <dbejarano820@gmail.com>
2023-04-06 15:44:48 -0600

    initial router and app frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import CourseList from "./components/Course/CourseList";
+import StudentList from "./components/Student/StudentList";
+import MatriculaList from "./components/StudentCourse/StudentCourseList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/courses">Cursos</Link>
+            </li>
+            <li>
+              <Link to="/students">Estudiantes</Link>
+            </li>
+            <li>
+              <Link to="/matricula">Matricula</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/courses">
+            <CourseList />
+          </Route>
+          <Route path="/students">
+            <StudentList />
+          </Route>
+          <Route path="/matricula">
+            <Matricula />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
