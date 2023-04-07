642341ff29caf2344104f7a703f8b20e1c6354b1
Daniel Bejarano <dbejarano820@gmail.com>
2023-04-07 16:43:21 -0600

    initial style components integration

--- frontend/src/components/Course/CourseList.js
+++ frontend/src/components/Course/CourseList.js
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
 import { getCourses, createCourse, updateCourse, deleteCourse } from "../../api";
 import CourseListItem from "./CourseListItem";
 import CourseForm from "./CourseForm";
+
+const StyledContainer = styled.div`
+  width: 100%
+  padding: 15px;
+  background-color: #fff;
+`;
+
+const StyledHeader = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+  margin: 10px;
+`;
 
 const CourseList = () => {
   const [courses, setCourses] = useState([]);
@@ -31,13 +44,13 @@ const CourseList = () => {
   };
 
   return (
-    <div>
-      <h2>Courses</h2>
+    <StyledContainer>
+      <StyledHeader>Courses</StyledHeader>
       <CourseForm onSubmit={handleCreate} />
       {courses.map((course) => (
         <CourseListItem key={course.id} course={course} onUpdate={handleUpdate} onDelete={handleDelete} />
       ))}
-    </div>
+    </StyledContainer>
   );
 };
 
