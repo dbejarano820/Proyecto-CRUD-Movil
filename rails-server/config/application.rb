f90c8b652340b9d781e8d42f55923398e457f58e
Daniel Bejarano <dbejarano820@gmail.com>
2023-04-10 10:06:28 -0600

    cors ip update for rails server

--- rails-server/config/application.rb
+++ rails-server/config/application.rb
@@ -42,7 +42,7 @@ module RailsServer
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', '127.0.0.1:3000'
+        origins '*'
     
         resource '*',
           headers: :any,
